12622764bfa10bd28b7d1c40bda22f6c5438dd3f
Lorin Steel <lorinsteel@gmail.com>
2016-01-27 14:20:18 -0700

    updated to pass tests

--- problems/07-sum.js
+++ problems/07-sum.js
@@ -1,5 +1,7 @@
 'use strict';
 
 module.exports = function sum(numArray) {
-
+  return numArray.reduce(function(total, curr){
+    return (typeof(curr) !== 'number' ? total : total + curr );
+  }, 0);
 };
--- problems/08-highest.js
+++ problems/08-highest.js
@@ -1,5 +1,7 @@
 'use strict';
 
 module.exports = function highest(numArray) {
-
+  return numArray.reduce(function(highest, curr){
+    return (typeof(curr) === 'number' && curr > highest || highest == null ? curr : highest );
+  }, null);
 };
